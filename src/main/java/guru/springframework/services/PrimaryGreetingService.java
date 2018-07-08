bee11cb4595223fc5beb37feb19c2e867fd007ef
praveen-vp <praveen_v.p@outlook.com>
2018-07-08 21:11:10 +0530

    Adding profile annotation in PrimaeryGreetingService

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,10 +1,12 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile(value = { "primary" })
 public class PrimaryGreetingService implements GreetingService {
 
 	@Override
