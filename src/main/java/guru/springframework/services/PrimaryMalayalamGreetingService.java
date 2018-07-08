de829588eba41222506dc3a1182b2c9029f8ebd7
praveen-vp <praveen_v.p@outlook.com>
2018-07-08 21:03:43 +0530

    Adding src/main/java/guru/springframework/services/PrimaryMalayalamGreetingService.java

---
+++ src/main/java/guru/springframework/services/PrimaryMalayalamGreetingService.java
@@ -0,0 +1,17 @@
+package guru.springframework.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile(value = { "ml" })
+public class PrimaryMalayalamGreetingService implements GreetingService {
+
+	@Override
+	public String sayGreeting() {
+		return "Hello -- Primary Malayalam Greeting service";
+	}
+
+}
