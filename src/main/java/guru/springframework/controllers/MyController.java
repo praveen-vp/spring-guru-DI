da961ea93a67ac5509e2147d3cc396d0f6a1f58b
praveen-vp <praveen_v.p@outlook.com>
2018-07-08 18:20:57 +0530

    Addin changes in MyController

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -2,12 +2,23 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.services.GreetingService;
+
 @Controller
 public class MyController {
+	
+	private GreetingService greetingService;
+
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
 
 	public String hello() {
-		System.out.println("Hello !");
-		return "Hi";
+		
+		System.out.println("Hello !!! -- MyController ");
+		
+		return greetingService.sayGreeting();
 	}
 
 }
