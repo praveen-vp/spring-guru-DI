94ff2aa62258e79c94743581f230d6610ee2e40f
praveen-vp <praveen_v.p@outlook.com>
2018-07-08 21:35:08 +0530

    Adding details in PrimaryGreetingService

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -4,9 +4,25 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+/**
+ * 
+ * @author praveen-vp 08-Jul-2018
+ * 
+ *         Added default profile for the GreetingService. If no profiles are
+ *         defined then the default profile will be taken, As there is no
+ *         qualifier for the Mycontroller and there are multiple primary
+ *         implementations, namely PrimaryGreetingService,
+ *         PrimaryMalayalamGreetingService and PrimaryGermanGreetingService.
+ * 
+ *         All the three services have a profile assosiated with it. If no
+ *         profile is specified in the application.properties then the 'default'
+ *         profile will be taken and the PrimaryGreetingService will be
+ *         implemented.
+ * 
+ */
 @Service
 @Primary
-@Profile(value = { "primary" })
+@Profile(value = { "primary", "default" })
 public class PrimaryGreetingService implements GreetingService {
 
 	@Override
