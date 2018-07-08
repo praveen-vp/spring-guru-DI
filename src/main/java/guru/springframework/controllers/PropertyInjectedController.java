2b0d24462757a69714b67ba5e8823f9ba33f8ac6
praveen-vp <praveen_v.p@outlook.com>
2018-07-08 18:19:41 +0530

    chaging the GreetingserviceImp to GreetingService

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -3,15 +3,15 @@ package guru.springframework.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import guru.springframework.services.GreetingServiceImp;
+import guru.springframework.services.GreetingService;
 
 @Controller
 public class PropertyInjectedController {
 
 	@Autowired
-	public GreetingServiceImp greetingService;
+	public GreetingService greetingServiceImp;
 
 	public String sayHello() {
-		return greetingService.sayGreeting();
+		return greetingServiceImp.sayGreeting();
 	}
 }
