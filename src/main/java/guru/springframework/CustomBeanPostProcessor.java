ab8e83612f93c99bbf1db336d56dc037e98d6232
praveen-vp <praveen_v.p@outlook.com>
2018-07-14 12:01:08 +0530

    change for bean life cycle

---
+++ src/main/java/guru/springframework/CustomBeanPostProcessor.java
@@ -0,0 +1,35 @@
+package guru.springframework;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author praveen-vp
+ * 11-Jul-2018
+ *
+ */
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+		if (bean instanceof LifeCycleDemoBean) {
+			((LifeCycleDemoBean) bean).beforeInit();
+		}
+
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+		if (bean instanceof LifeCycleDemoBean) {
+			((LifeCycleDemoBean) bean).afterInit();
+		}
+
+		return bean;
+	}
+}
--- src/main/java/guru/springframework/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/LifeCycleDemoBean.java
@@ -1,5 +1,8 @@
 package guru.springframework;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -8,57 +11,66 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+/**
+ * 
+ * @author praveen-vp
+ * 11-Jul-2018
+ *
+ */
+@Component
 public class LifeCycleDemoBean
 		implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
 
 	public LifeCycleDemoBean() {
-		System.out.println("in LifeCycleDemoBean constructor");
+		System.out.println("##<><>##I'm in LifeCycleDemoBean constructor");
 	}
 
 	public void beforeInit() {
-
+		System.out.println("##<><>## Before init method calle by bean post processor");
 	}
 
 	public void afterInit() {
-
+		System.out.println("##<><>## After init method calle by bean post processor");
 	}
 
+	@PostConstruct
 	public void postConstruct() {
-
+		System.out.println("##<><>##Post construct annotated method has been called.");
 	}
 
+	@PreDestroy
 	public void preDestroy() {
-
+		System.out.println("##<><>##PreDestroy annotated method has been called.");
 	}
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		System.out.println("in LifeCycleDemoBean constructor");
+		System.out.println("##<><>##in LifeCycleDemoBean constructor");
 
 	}
 
 	@Override
 	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-		// TODO Auto-generated method stub
+		System.out.println("##<><>##bean factory has been set");
 
 	}
 
 	@Override
 	public void setBeanName(String name) {
-		// TODO Auto-generated method stub
+		System.out.println("##<><>##bean name is : " + name);
 
 	}
 
 	@Override
 	public void destroy() throws Exception {
-		// TODO Auto-generated method stub
-
+		System.out.println("##<><>##in LifeCycleDemoBean destroyed");
 	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("##<><>## LifeCycleDemoBean has properties set.");
 
 	}
 
